77959844b3e1fb8edad52df4b80716d9ddba6858
jongseob <cjsgkwkd4@gmail.com>
2023-05-17 17:42:01 +0900

    remove uvm-1.1d including. because questasim includes uvm directiry as default

--- hello-uvm-questasim-windows/run.bat
+++ hello-uvm-questasim-windows/run.bat
@@ -1,5 +1,5 @@
 @echo off
 
-vlog -writetoplevels questa.tops -timescale 1ns/1ns -L /usr/share/questa/questasim/uvm-1.1d design.sv testbench.sv
+vlog -writetoplevels questa.tops -timescale 1ns/1ns design.sv testbench.sv
 echo "-- Compiling module tb"
 vsim tb -batch -do "vsim -voptargs=+acc=npr; run -all; exit" -voptargs="+acc=npr"
