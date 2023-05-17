08df0d0d989c3ae9ea13579ecf40de01a2a00c50
jongseob <cjsgkwkd4@gmail.com>
2023-05-17 21:18:54 +0900

    improved dll creation code with for loop and glob

--- hello-vpi-questasim-windows/run.bat
+++ hello-vpi-questasim-windows/run.bat
@@ -1,21 +1,27 @@
 @echo off
-REM http://www.pldworld.com/_hdl/2/_ref/se_html/manual_html/c_vlog45.html
-REM https://www.microsemi.com/document-portal/doc_view/131619-modelsim-user, 662 page
-echo "============================================"
-echo "run.bash started!!!"
-echo "============================================"
 del *.o *.dll
+cls
 
-echo "============================================"
-echo "run gcc to create \"libvpi.dll\""
-echo "============================================"
+echo -----------------------------------------
+echo   Create "libvpi.dll" with MinGW gcc
+echo -----------------------------------------
+REM Define common C Compiler options & Linker Directives for VPI
 set "CC=-D__MINGW32__ -g -c -m64 -Wall -D__USE_MINGW_ANSI_STDIO=1 -I. -IC:\questasim64_2021.1\include "
-set "LD=-shared -lm -m64 -Wl,-Bsymbolic -Wl,-export-all-symbols -o "
+set "LD=-shared -lm -m64 -Wl,-Bsymbolic -Wl,-export-all-symbols "
 
-gcc %CC% -c vpi_user.c -o vpi_user.o
-gcc %CC% -c hello_vpi.c -o hello_vpi.o
-gcc %LD% libvpi.dll vpi_user.o hello_vpi.o C:\questasim64_2021.1\win64\mtipli.lib
+REM Iterates all C files in the current directory
+for %%F in (*.c) do (
+    REM Compile each C file
+    echo -- Compiling %%F...
+    gcc %CC% -c %%F -o %%~nF.o
+)
 
-vlog -writetoplevels questa.tops -timescale 1ns/1ns -L /usr/share/questa/questasim/uvm-1.1d design.sv testbench.sv
-echo "-- Compiling module tb"
+echo -- Linking...
+gcc %LD% -o libvpi.dll *.o C:\questasim64_2021.1\win64\mtipli.lib
+
+echo -----------------------------------------
+echo   Run VPI Sim using "libvpi.dll"
+echo -----------------------------------------
+vlog -writetoplevels questa.tops -timescale 1ns/1ns design.sv testbench.sv
+echo -- Running simulation
 vsim tb -batch -do "vsim -voptargs=+acc=npr; run -all; exit" -voptargs="+acc=npr" -pli libvpi.dll
