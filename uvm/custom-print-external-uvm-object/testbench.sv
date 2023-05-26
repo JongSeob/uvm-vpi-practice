0215df656c389072d2c3a68b4cf9403b9cfbfb56
JongSeob <cjsgkwkd4@gmail.com>
2023-05-26 10:06:37 +0900

    add print_generic example

--- uvm/custom-print-external-uvm-object/testbench.sv
+++ uvm/custom-print-external-uvm-object/testbench.sv
@@ -1,13 +1,9 @@
 `include "uvm_macros.svh"
 `include "macros/uvm_printer_defines.svh"
-
 import uvm_pkg::*;
-
 module tb;
   typedef enum {FALSE = 0, TRUE = 1} e_bool;
-  
   class my_obj_c extends uvm_object;
-    
     // NOTE
     // array	: dynamic array
     // sarray	: static array
@@ -47,7 +43,6 @@ module tb;
     int                 var_aa_int_key[int];
     //string              var_aa_string_int[int]; `uvm_field_* macro for aa_string_int does not exist
     uvm_object          var_aa_object_int[int];
-
     `uvm_object_utils_begin(my_obj_c)
         `uvm_field_int                      ( var_int                      , UVM_DEFAULT                  ) // `uvm_field_int                      ( ARG , FLAG       )
         `uvm_field_real                     ( var_real                     , UVM_DEFAULT                  ) // `uvm_field_real                     ( ARG , FLAG       )
@@ -55,10 +50,10 @@ module tb;
         `uvm_field_object                   ( var_object                   , UVM_DEFAULT                  ) // `uvm_field_object                   ( ARG , FLAG       )
         `uvm_field_event                    ( var_event                    , UVM_DEFAULT                  ) // `uvm_field_event                    ( ARG , FLAG       )
         `uvm_field_string                   ( var_string                   , UVM_DEFAULT                  ) // `uvm_field_string                   ( ARG , FLAG       )
-    	`uvm_field_array_enum               ( e_bool                       , var_array_enum , UVM_DEFAULT ) // `uvm_field_array_enum               ( T   , ARG , FLAG )
+    	  `uvm_field_array_enum               ( e_bool                       , var_array_enum , UVM_DEFAULT ) // `uvm_field_array_enum               ( T   , ARG , FLAG )
         `uvm_field_array_int                ( var_array_int                , UVM_DEFAULT                  ) // `uvm_field_array_int                ( ARG , FLAG       )
         `uvm_field_sarray_int               ( var_sarray_int               , UVM_DEFAULT                  ) // `uvm_field_sarray_int               ( ARG , FLAG       )
-    	`uvm_field_sarray_enum              ( e_bool                       , var_sarray_enum, UVM_DEFAULT ) // `uvm_field_sarray_enum              ( T   , ARG , FLAG )
+    	  `uvm_field_sarray_enum              ( e_bool                       , var_sarray_enum, UVM_DEFAULT ) // `uvm_field_sarray_enum              ( T   , ARG , FLAG )
         `uvm_field_array_object             ( var_array_object             , UVM_DEFAULT                  ) // `uvm_field_array_object             ( ARG , FLAG       )
         `uvm_field_sarray_object            ( var_sarray_object            , UVM_DEFAULT                  ) // `uvm_field_sarray_object            ( ARG , FLAG       )
         `uvm_field_array_string             ( var_array_string             , UVM_DEFAULT                  ) // `uvm_field_array_string             ( ARG , FLAG       )
@@ -84,13 +79,10 @@ module tb;
       //`uvm_field_aa_string_int            ( var_aa_string_int            , UVM_DEFAULT                  ) // `uvm_field_aa_string_int            ( ARG , FLAG       )
         `uvm_field_aa_object_int            ( var_aa_object_int            , UVM_DEFAULT                  ) // `uvm_field_aa_object_int            ( ARG , FLAG       )
     `uvm_object_utils_end
-    
     function new (string name = "");
       super.new(name);
-      
       init_properties();
     endfunction
-    
     function void init_properties();
       var_enum = TRUE;
       var_object = null;
@@ -99,33 +91,28 @@ module tb;
       var_array_int    = new[5]({0, 1, 2, 3, 4});
       var_array_object = new[5]({null, null, null, null, null});
       var_array_string = new[5]({"1", "2", "3", "4", "5"});
-
       var_aa_int_string["ten"]    = 10;
       var_aa_int_string["eleven"] = 11;
       var_aa_int_string["twenty"] = 20;
 	endfunction: init_properties
-        
   endclass
-  
   function void my_print(my_obj_c obj);
 	// refer to uvm_1.1d/sv/src/base/uvm_object.svh, Line 875, uvm_object::print
     automatic uvm_table_printer tprinter = new();
-    
     tprinter.print_int        ("var_int",                      obj.var_int, $bits(int), UVM_HEX);
     tprinter.print_real       ("var_real",                     obj.var_real);
     tprinter.print_generic    ("var_enum",                     "e_bool", $bits(e_bool), obj.var_enum.name());
     tprinter.print_object_header ( "var_object", obj.var_object );
     tprinter.print_generic    ("var_event",                    "event", -1, "");
     tprinter.print_string     ("var_string",                   obj.var_string);
-    
     begin
       tprinter.print_array_header ("var_array_enum", obj.var_array_enum.size(), "da(e_bool)");
       foreach(obj.var_array_enum[i])
         tprinter.print_generic($sformatf("[%0d]", i), "e_bool", $bits(e_bool), obj.var_array_enum[i].name(), "["); // last argument is scope_separator
       tprinter.print_array_footer (obj.var_array_enum.size());													   // characters in front of scope_separator is not printed.
-    end    
+    end
     begin
-      tprinter.print_array_header ("var_array_int", obj.var_array_int.size(), "da(int)");    
+      tprinter.print_array_header ("var_array_int", obj.var_array_int.size(), "da(int)");
       foreach(obj.var_array_int[i])
         tprinter.print_generic($sformatf("[%0d]", i), "int", $bits(int), $sformatf("'h%0h", obj.var_array_int[i]), "[");
         tprinter.print_array_footer (obj.var_array_int.size());
@@ -142,16 +129,14 @@ module tb;
         tprinter.print_generic($sformatf("[\"%s\"]", key), "int", $bits(int), $sformatf("'h%0h", obj.var_aa_int_string[key]), "[");
       tprinter.print_array_footer ($size(obj.var_aa_int_string));
     end
-
+    // if the third argument of print_generic() "size" is set to -2, it prints "..." instead of -2.
+    tprinter.print_generic    ("...", "...", -1, "..."); // ...                ...              -1    ...
+    tprinter.print_generic    ("...", "...", -2, "..."); // ...                ...              ...   ...
     $fwrite(tprinter.knobs.mcd, tprinter.emit());
   endfunction
-  
   initial begin
     my_obj_c my_obj = new;
-    
     my_obj.print();
-    
     my_print(my_obj);
   end
-  
 endmodule
